e709336701c9d1f6cb44865581a3801f5d551232
Dave Nash <davidjnash2@gmail.com>
2023-06-02 20:32:15 -0500

    added images to image file, updated array to hold them, and added render placeholders for components to appjsx

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,14 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/snow.jpeg', description: 'I like bass.', likes: 0 },
+    { id: 2, path: 'images/vote.jpeg', description: 'I like sports.', likes: 0 },
+    { id: 3, path: 'images/bass.jpeg', description: 'I like fair.', likes: 0 },
+    { id: 4, path: 'images/fair.jpeg', description: 'I like hike.', likes: 0 },
+    { id: 5, path: 'images/hike.jpeg', description: 'I like snow.', likes: 0 },
+    { id: 6, path: 'images/levifish.jpeg', description: 'I like vote.', likes: 0 },
+    { id: 7, path: 'images/bball.jpeg', description: 'Levi like fish.', likes: 0 }
 ];
 
 module.exports = galleryItems;
+
+
+
